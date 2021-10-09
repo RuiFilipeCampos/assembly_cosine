bee01f524310ff7116c00b21cfad239400ad9ade
unknown <63464503+RuiFilipeCampos@users.noreply.github.com>
2021-10-09 19:30:54 +0100

    fun stuff

--- assembly_cosine/cosine.asm
+++ assembly_cosine/cosine.asm
@@ -1,4 +1,12 @@
-
+.data
+coef0 QWORD 1
+coef1 QWORD -0.5
+coef2 QWORD 0.04166666666
+coef3 QWORD -0.00138888888
+coef4 QWORD 0.00002480158
+coef5 QWORD 0
+coef6 QWORD 0
+coef7 QWORD 0
 
 .code
 cosine proc 
@@ -8,41 +16,35 @@ cosine proc
 
 	movq  xmm1, xmm0   ; xmm1 = x
 	mulsd xmm1, xmm1   ; xmm1 = x**2
-
-	movq xmm2, xmm0  ; xmm2 = x**2 (tmp)
+	movq xmm2, xmm1  ; xmm2 = x**2 (tmp)
 
 	; filling xmm0 with the value of 1 -> the starting point of the taylor series
-	mov rax, 1
-	movq xmm0, rax
+	
+	; xmm0 = 1
+	movq xmm0, coef0
 
-	; x**2
-	; coef = - 1/2
+	; xmm2 = x**2
+	movq xmm3, xmm2   ; so. many. copies. :(
+	mulsd xmm3, coef1 ; xmm2 = coef1*x**2 
+	addsd xmm0, xmm3  ; xmm0 = 1 + coef1*x**2
 
-	mov rax, -1/2
-	movq xmm3, rax
-	mulsd xmm3, xmm2
+	; xmm2 = x**4
+	mulsd xmm2, xmm1
+	movq xmm3, xmm2
+	mulsd xmm3, coef2
 	addsd xmm0, xmm3
 
-
-	mulsd xmm2, xmm1 ; x**4
-	mov rax, -1/2
-	movq xmm3, rax
-	mulsd xmm3, xmm2
+	; xmm2 = x**4
+	mulsd xmm2, xmm1
+	movq xmm3, xmm2
+	mulsd xmm3, coef3
 	addsd xmm0, xmm3
 
-
-
-	mulsd xmm2, xmm1 ; x**6
-	addsd xmm0, xmm2
-
-	mulsd xmm2, xmm1 ; x**8
-	addsd xmm0, xmm2
-
-	mulsd xmm2, xmm1 ; x**10
-	addsd xmm0, xmm2
-
-	mulsd xmm2, xmm1 ; x**12
-	addsd xmm0, xmm2
+	; xmm2 = x**4
+	mulsd xmm2, xmm1
+	movq xmm3, xmm2
+	mulsd xmm3, coef4
+	addsd xmm0, xmm3
 
 	ret
 cosine endp
--- assembly_cosine/main.cpp
+++ assembly_cosine/main.cpp
@@ -5,6 +5,6 @@ using namespace std;
 extern "C" double cosine(double);
 
 int main() {
-	cout << "The result is " << cosine(2.) << endl;
+	cout << "The result is " << cosine(1.57079632679) << endl;
 	return 0; 
 };
