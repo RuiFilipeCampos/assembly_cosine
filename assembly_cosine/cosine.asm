79a135799abeebc7c7aba70c58cb46a0252b7bdc
unknown <63464503+RuiFilipeCampos@users.noreply.github.com>
2021-10-09 14:37:55 +0100

    another register being used

--- assembly_cosine/cosine.asm
+++ assembly_cosine/cosine.asm
@@ -1,34 +1,49 @@
-.data
-CONSTANTS QWORD 0, 1, 2, 3, 4, 5
 
 
 .code
 cosine proc 
 	; res = xmm0
-	; x   = xmm1
-	; x^n = xmm2
+	; x**2   = xmm1
+	; x^n   = xmm2
 
-	movq xmm1, xmm0 ; defining x
-	movq xmm2, xmm0
+	movq  xmm1, xmm0   ; xmm1 = x
+	mulsd xmm1, xmm1   ; xmm1 = x**2
+
+	movq xmm2, xmm0  ; xmm2 = x**2 (tmp)
 
 	; filling xmm0 with the value of 1 -> the starting point of the taylor series
 	mov rax, 1
 	movq xmm0, rax
 
-	; x
+	; x**2
+	; coef = - 1/2
 
-	mulsd xmm2, xmm1 ; x**2
+	mov rax, -1/2
+	movq xmm3, rax
+	mulsd xmm3, xmm2
+	addsd xmm0, xmm3
 
-	mulsd xmm2, xmm1 ; x**3
 
 	mulsd xmm2, xmm1 ; x**4
+	mov rax, -1/2
+	movq xmm3, rax
+	mulsd xmm3, xmm2
+	addsd xmm0, xmm3
 
-	mulsd xmm2, xmm1 ; x**5
+
 
 	mulsd xmm2, xmm1 ; x**6
+	addsd xmm0, xmm2
 
+	mulsd xmm2, xmm1 ; x**8
+	addsd xmm0, xmm2
 
-	movq xmm0, xmm2
+	mulsd xmm2, xmm1 ; x**10
+	addsd xmm0, xmm2
+
+	mulsd xmm2, xmm1 ; x**12
+	addsd xmm0, xmm2
+
 	ret
 cosine endp
 end
